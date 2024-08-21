7d74b6f18466ca8650c99411dd8e262babe30f2e
robocop24 <suhailriyaz94@gmail.com>
2024-08-21 23:14:25 +0530

    task 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([])
+
+  const fetchData = async () => {
+    let a = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let data = await a.json()
+    setCards(data)
+    console.log(data)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        {cards.map((card)=>{
+          return <div key={card.id} className="card">
+            <h1>{card.title}</h1>
+            <p>{card.body}</p>
+            <span>By: UserId: {card.userId}</span>
+          </div>
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
